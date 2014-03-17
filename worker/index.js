25b4a832bb67058d2f7ef234b2b35e594ddf5008
vaxann <vaxann@gmail.com>
2014-03-17 18:07:24 +0400

    Modified worker class

--- worker/index.js
+++ worker/index.js
@@ -1,12 +1,18 @@
 var events = require('events');
 var log = require('../log')(module);
 var exec = require('child_process').exec;
+var util = require('util');
 
 function Worker(transmission, job, file) {
     log.info('Создаем объект работник Worker с параметрами: transmission-remote=%j, job=%j file=%s', transmission, job, file);
     var Worker = this;
 
-    exec('transmission-remote localhost:9091 --auth=ivanabramenko:frtgbfrtgb -si',
+    Worker.addTorrentCommand = util.format('transmission-remote %s:%d --auth=%s -si',
+                                                transmission.host,
+                                                transmission.port,
+                                                transmission.auth);
+
+    exec(Worker.addTorrentCommand,
         function (error, stdout, stderr) {
             log.info(stdout);
             log.error(stderr);
